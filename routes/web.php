a5ab4021bdec91008255a72a927079af8208f181
Ricardo Moreira <calditos1988@outlook.com>
2021-05-07 23:58:32 -0300

    criando as rotas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,29 @@
 <?php
+use App\Http\Controllers\Admin\TesteController;
 
 
+Route::get('/login', function () {
+    return 'login';
+})->name('login');
 
-Route::get('/', function () {
-    return view('welcome');
+    Route::middleware([])->group(function () {
+
+      Route::prefix('admin')->group(function () {
+        Route::get('/dashboard', 'Admin\TesteController@teste');
+
+        Route::get('/financeiro', 'Admin\TesteController@teste');
+
+        Route::get('/rh', 'Admin', 'Admin\TesteController@teste');
+
+        Route::get('/', 'Admin\TesteController@teste');
+
+        Route::get('/', function () {
+            return view('welcome');
+        });
+
+      });
+
 });
+
+
+
